bdd90eb048e0415d2a84de9318c3726c3bc97745
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-04 11:28:51 +0530

    user login authentication

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+use App\Http\Controllers\AuthController;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
