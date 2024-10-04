220e4f13e72d81db0bfc6afb5519f5b4c6ac9939
chamalanuradha <chamalanuradha98765@gmail.com>
2024-10-04 13:03:27 +0530

    product crud application backend

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function store(Request $request)
+    {
+        $product = Product::create($request->all());
+        return response()->json($product, 201);
+    }
+
+
+    public function index()
+    {
+        $products = Product::all();
+        return response()->json($products);
+    }
+
+    public function show($id)
+    {
+        $product = Product::find($id);
+        return response()->json($product);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $product = Product::find($id);
+        $product->update($request->all());
+        return response()->json($product);
+    }
+
+    public function destroy($id)
+    {
+        Product::destroy($id);
+        return response()->json(['message' => 'Product deleted']);
+    }
+}
+
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,12 @@ use App\Http\Controllers\AuthController;
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
+
+use App\Http\Controllers\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::get('/products/{id}', [ProductController::class, 'show']); 
+Route::post('/products', [ProductController::class, 'store']);
+Route::put('/products/{id}', [ProductController::class, 'update']);  
+Route::delete('/products/{id}', [ProductController::class, 'destroy']); 
+
